067cdbb9ca20a8548ab733a5665980ecc4da5fd7
Walter Wang <wzc314159@hotmail.com>
2021-11-19 08:07:31 +0800

    add rollup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("hello sdfsadf" + "abc");
+const func = () => {
+  const stu = { name: 'walter', age: 19 }
+  const stuName = stu?.name
+  const stuAge = stu?.age
+  return 'name: ' + stuName + ' age:: ' + stuAge
+}
+
+export default func
