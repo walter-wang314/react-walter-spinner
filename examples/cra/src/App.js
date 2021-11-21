2aadd1a38170e351b411504bdd4667303787f773
Walter Wang <wzc314159@hotmail.com>
2021-11-21 17:07:53 +0800

    add jest snapshot

--- examples/cra/src/App.js
+++ examples/cra/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Ellipsis } from 'react-walter-spinner';
+import { Ellipsis, Ring } from 'react-walter-spinner';
 function App() {
   return (
     <div>
       <h1>Hello</h1>
       <div>
         <Ellipsis />
+      </div>
+      <div>
+        <Ring />
       </div>
     </div>
   );
